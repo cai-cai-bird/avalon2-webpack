6518d616c3e56e780c66fc973667ab46614751a3
Chen <236514977@qq.com>
2016-07-18 18:03:47 +0800

    !demo

---
+++ app/_js/avalon2-demo.js
@@ -0,0 +1,45 @@
+require("../_scss/avalon2-demo.scss");
+/*ms-controller*/
+avalon.define({
+    $id: "AAA",
+    name: "liger",
+    color: "green"
+});
+avalon.define({
+    $id: "BBB",
+    name: "sphinx",
+    color: "red"
+});
+avalon.define({
+    $id: "CCC",
+    name: "dragon", //不存在color
+    color: "blue"
+
+});
+avalon.define({
+    $id: "DDD",
+    name: "sirenia" //不存在color
+
+});
+
+/*ms-important*/
+avalon.define({
+    $id: "aaa",
+    name: "liger",
+    color: "green"
+});
+avalon.define({
+    $id: "bbb",
+    name: "sphinx",
+    color: "red"
+});
+avalon.define({
+    $id: "ccc",
+    name: "dragon" //不存在color
+
+});
+avalon.define({
+    $id: "ddd",
+    name: "sirenia" //不存在color
+
+});
